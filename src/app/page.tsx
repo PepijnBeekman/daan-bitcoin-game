2d1c927f47f858ad9a78fdc929527d876a282506
PepijnBeekman <pepijn@ecsens.com>
2025-12-01 00:46:26 +0100

    bkg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -249,7 +249,7 @@ Time to pay up! 💰
           Daan&apos;s Birthday Bitcoin Minigame
         </h1>
         <p className="text-sm text-center text-slate-300 mb-6">
-          Daan, we love you but we refuse to just give you money for your birthday. That's just not our  love language. But ok, it's your birthday so I guess we can lend you some investment budget to *earn* your gift. Buy low, sell high! Hint: mind the transaction fees!
+          Happy birthday Daan!!!! Hey, we love you but we refuse to just give you money for your birthday. That's just not our love language. But ok, since it's your birthday so I guess we can lend you some investment budget to *earn* your gift. Buy low, sell high! Hint: mind the transaction fees!
         </p>
 
         <div className="mb-4 flex flex-col items-center">
@@ -317,7 +317,7 @@ Time to pay up! 💰
 
         <p className="mt-6 text-[11px] text-slate-500 text-center">
           This is a fictional game. No real Bitcoin is harmed. Real money may
-          be harmed if you play well enough.
+          be harmed if you play well enough - feel fry to try again if not.
         </p>
       </div>
     </main>
